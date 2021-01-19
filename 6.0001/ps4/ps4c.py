85a2f2b3845c55728b9353775a1edc03939b3de7
Lewis-C-L <57136095+Lewis-C-L@users.noreply.github.com>
2021-01-19 17:42:21 +0800

    deleted get_permutations

--- 6.0001/ps4/ps4c.py
+++ 6.0001/ps4/ps4c.py
@@ -53,38 +53,6 @@ def is_word(word_list, word):
 
 ### END HELPER CODE ###
 
-### Functions from part A ###
-def get_permutations(sequence):
-    '''
-    Enumerate all permutations of a given string
-
-    sequence (string): an arbitrary string to permute. Assume that it is a
-    non-empty string.  
-
-    You MUST use recursion for this part. Non-recursive solutions will not be
-    accepted.
-
-    Returns: a list of all permutations of sequence
-
-    Example:
-    >>> get_permutations('abc')
-    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
-
-    Note: depending on your implementation, you may return the permutations in
-    a different order than what is listed here.
-    '''
-
-    letters = [x for x in sequence]
-    pos = 1
-    new_sequences = [sequence[0]]
-    while pos<len(letters):
-        l = [[y[:x]+letters[pos]+y[x:] for x in range(pos+1)] for y in new_sequences]
-        new_sequences = list(flatten(l))
-        pos+=1
-    return new_sequences
-
-### END OF FUCNTIONS FROM PART A ###
-
 
 WORDLIST_FILENAME = 'words.txt'
 
